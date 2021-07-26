cd120c439ba1b79bb0fc3d622a6c25c865c79c97
Simon Postnikov <postnikov.sn@mail.ru>
2021-07-26 22:05:45 +0300

    git commit -m"Remove Configuration.headless = false"

--- GithubSelenide/src/test/java/FindSelenideTest.java
+++ GithubSelenide/src/test/java/FindSelenideTest.java
@@ -1,21 +1,18 @@
-import com.codeborne.selenide.*;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FindSelenideTest {
 
     @Test
     void SoftAssertionsTes() {
-        Configuration.headless = false;
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").$(byText("Wiki")).click();
-        //$(".js-wiki-more-pages-link").scrollTo().click();
-        //$$("#wiki-pages-box").findBy(text("SoftAssertions")).scrollTo().click();
-        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $(".js-wiki-more-pages-link").scrollTo().click();
         $("#wiki-pages-box").find(byText("SoftAssertions")).click();
+        sleep(5000);
         $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
     }
 
